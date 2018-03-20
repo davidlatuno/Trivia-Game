ab7ea9813f86fec6c1e157e5c3a87f88154bf8ce
davidlatuno <dslatuno@gmail.com>
2018-03-19 19:04:53 -0700

    Added some example questions and jquery click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,36 @@
+var question = {
+
+    q1: ["What is the only manmade object that is observable from the moon?", "Me", "The Sun", "The Great Wall", "The not so great wall", 3],
+
+    q2: ["What is the capital of Australia", "Me", "The Sun", "The Great Wall", "Canberra", 4],
+
+}
+
+var questionArray = [question.q1, question.q2];
+
+var questionIndex = 0;
+
+function init() {
+
+    if (questionIndex <= questionArray.length -1) {
+
+        $("#question").text(questionArray[questionIndex][0]);
+        $("#answer1").text(questionArray[questionIndex][1]);
+        $("#answer2").text(questionArray[questionIndex][2]);
+        $("#answer3").text(questionArray[questionIndex][3]);
+        $("#answer4").text(questionArray[questionIndex][4]);
+
+    } else {
+
+        $("#question").text("Game Over");
+
+    }
+
+}
+
+
+init();
+
 var seconds = 30;
 
 var timer;
@@ -10,9 +43,9 @@ function start() {
 
 function countDown() {
 
-    seconds--;
-
     $("#timer").text("Time Remaining: " + seconds + " Seconds");
+
+    seconds--;
 
     if (seconds === 0) {
 
@@ -30,3 +63,18 @@ function stop() {
 }
 
 start();
+
+
+$(document).ready(function() {
+
+
+    $(".answers").on("click", ".answer", function() {
+
+        console.log($(this).attr("value"));
+
+
+    })
+
+
+
+})
